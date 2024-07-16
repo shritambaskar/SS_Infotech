68f1b7320dce63433b28da888524dca87e5e13cd
Technocrat Solutions <shri.tambaskar@gmail.com>
2024-07-16 17:07:48 +0530

    JavaScript Added

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log(`Hello SS INFOTECH`);
+
+let a =90;
+let b = 10;
+
+a = 30;
+let c = a+b;
+console.log(c);
+
+var cityCOde = 440012;
+// console.log(cityCOde);
+var cityCOde = 440039;
+console.log(cityCOde);
+
+const pin = 1234;
+// const pin = 9089;
+
+let name = `SS`;
+
+let students = {
+    name: `SS`,
+    age: 20,
+    city: `Hyderabad`,
+    pin: 1234,
+    address: `Hyderabad`,
+    phone: 1234567890
+}
+
+console.log(students.city);
+
+let thunder = {
+    
+        message: 'Welcome to Thunder Client',
+        "about": "Lightweight Rest API Client for VSCode",
+        "createdBy": "Ranga Vadhineni",
+        "launched": 2021,      
+}
